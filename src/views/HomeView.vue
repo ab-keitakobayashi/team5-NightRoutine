0ff3d35efdfe8e4314479a75bf9a2c9f00682294
keitakobayashi <keitakobayashi@abeam.com>
2025-06-23 11:48:22 +0900

    ルーティングを追加

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <v-col col="4">
+        <v-card>
+          <v-card-title class="text-h5">test</v-card-title>
+          <v-card-text>
+            <p>test</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col col="4">
+        <v-card>
+          <v-card-title class="text-h5">test</v-card-title>
+          <v-card-text>
+            <p>test</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col col="4">
+        <v-card>
+          <v-card-title class="text-h5">test</v-card-title>
+          <v-card-text>
+            <p>test</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
