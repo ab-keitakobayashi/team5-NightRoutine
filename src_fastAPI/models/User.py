861ed832cffed71679401aab2125d887d016d5d3
kosiwai <kosiwai@abeam.com>
2025-06-24 17:45:09 +0900

    モデルをbasemodelを継承するように

--- src_fastAPI/models/User.py
+++ src_fastAPI/models/User.py
@@ -1,9 +1,8 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
-
-class User(Base):
+class User(BaseModel):
     __tablename__ = "users"
 
     user_id = Column(Integer, primary_key=True, index=True)
