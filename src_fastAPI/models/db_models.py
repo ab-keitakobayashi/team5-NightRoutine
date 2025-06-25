0cba714026deda9d228bdf0e33446e554a81aeb8
kaztakeuchi <kaztakeuchi@abeam.com>
2025-06-25 11:01:36 +0900

    変更

--- src_fastAPI/models/db_models.py
+++ src_fastAPI/models/db_models.py
@@ -28,3 +28,20 @@ class ItemModel(Base):
     ef_item_id3 = Column(Integer, nullable=False)
     ef_item_id4 = Column(Integer, nullable=False)
     ef_item_id5 = Column(Integer, nullable=False)
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    user_name = Column(String, nullable=False)
+    user_mailAddress = Column(String, unique=True, nullable=False)
+    class_id = Column(Integer, nullable=False)
+    period = Column(Integer, nullable=False)
+    avatar_id = Column(Integer, nullable=False)
+    enemy_id = Column(Integer, nullable=False)
+    enemy_hp = Column(Integer, nullable=False)
+    ef_item_id1 = Column(Integer, nullable=False)
+    ef_item_id2 = Column(Integer, nullable=False)
+    ef_item_id3 = Column(Integer, nullable=False)
+    ef_item_id4 = Column(Integer, nullable=False)
+    ef_item_id5 = Column(Integer, nullable=False)
