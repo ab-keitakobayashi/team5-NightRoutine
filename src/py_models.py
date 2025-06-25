97af1ed5cea980708bf84591a1fc094914a59a81
kaztakeuchi <kaztakeuchi@abeam.com>
2025-06-25 13:56:52 +0900

    db変更

--- src/py_models.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI, HTTPException, Depends
-from sqlalchemy import create_engine, Column, Integer, String, Float
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
-
-
-
