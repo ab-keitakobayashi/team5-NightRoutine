f0367cb666c08ca75d0e4ceda6e5c29ec398178d
kosiwai <kosiwai@abeam.com>
2025-06-25 10:42:35 +0900

    temp

---
+++ src_fastAPI/app/userRegi/regi.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, HTTPException, Depends
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from src_fastAPI.util.db_connect import get_db  # DBセッション取得関数
+from src_fastAPI.models import User  # Userモデル
+
+router = APIRouter()
+
+class UserResiRequest(BaseModel):
+    id = int
+    name = str
+    mailAddress = str
+    class_id = int
+    period = int
+    avatar_id = int
+    enemy_id = int
+    enemy_hp = int
+    ef_item_id1 = int
+    ef_item_id2 = int
+    ef_item_id3 = int
+    ef_item_id4 = int
+    ef_item_id5 = int
+
+# ④itemを追加（サンプル実装済み）
+@app.post("/user/regi", response_model=UserResiRequest)
+def create_item(user: UserResiRequest, db_session: Session = Depends(get_db)):
+    db_user = User(usr_name=user.name, user_mailAddress=user.mailAddress,
+                   class_id=user.class_id, period=user.period, avatar_id=user.avatar_id,
+                   enemy_id=user.enemy_id, enemy_hp=user.enemy_hp,
+                   ef_item_id1=user.ef_item_id1, ef_item_id2=user.ef_item_id2,
+                   ef_item_id3=user.ef_item_id3, ef_item_id4=user.ef_item_id4,
+                   ef_item_id5=user.ef_item_id5
+                   ) # idは自動採番されるため、リクエストには含めない
+    db_session.add(db_user)
+    db_session.commit()
+    db_session.refresh(db_user)
+    return UserResiRequest(id=db_user.id, name=db_user.name, mailAddress=db_user.mailAddress,
+                            class_id=db_user.class_id, period=db_user.period, avatar_id=db_user.avatar_id,
+                            enemy_id=db_user.enemy_id, enemy_hp=db_user.enemy_hp,
+                            ef_item_id1=db_user.ef_item_id1, ef_item_id2=db_user.ef_item_id2,
+                            ef_item_id3=db_user.ef_item_id3, ef_item_id4=db_user.ef_item_id4,
+                            ef_item_id5=db_user.ef_item_id5)
--- src_fastAPI/util/db_connect.py
+++ src_fastAPI/util/db_connect.py
@@ -7,7 +7,13 @@ from sqlalchemy.orm import sessionmaker, Session
 DB接続設定とモデル定義
 """
 # DB接続設定
-DATABASE_URL = "" ##URL
+DATABASE_URL = "mysql+pymysql://admin:fy26admin@fy26-training-handson-db.cxok2mc8wgeq.ap-northeast-1.rds.amazonaws.com:3306/handson" ##URL
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+class ItemModel(Base):
+    __tablename__ = "group5_users_test" # テーブル名
+    id = Column(Integer, primary_key=True) # 主キー
+    name = Column(String, index=True)
+    price = Column(Float)
