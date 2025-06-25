e13771fab10e919477f787dd441691a186cc6a4a
kaztakeuchi <kaztakeuchi@abeam.com>
2025-06-25 17:22:40 +0900

    ユーザー登録および更新機能を追加し、関連するモデルのフィールド名を修正

--- src/renew.py
+++ src/renew.py
@@ -1,26 +1,38 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import FastAPI ,Depends, HTTPException, Query
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
-from db_connect import engine #DBセッション取得関数
-from db_connect import get_db
-from models import User# Userモデル
-
-router = APIRouter()
-
-
+from db_connect import get_db  # DBセッション取得関数
+from models import User  # Userモデル
+ 
+ 
+app = FastAPI()
+ 
+ 
+ 
 class UserUpdateRequest(BaseModel):
-    user_id: int
     class_id: int
     period: int
-    efitem_id: int
-
-@router.put("/user/update/{user_id}", response_model=dict)
+    ef_item_id_1: int
+    ef_item_id_2: int
+    ef_item_id_3: int
+    ef_item_id_4: int
+    ef_item_id_5: int
+   
+ 
+ 
+@app.put("/user/update/{user_id}", response_model=dict)
 def update_user_profile(user_id: int, request: UserUpdateRequest, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.id == user_id).first()
-    if not user:
+    db_user = db.query(User).filter(User.user_id == user_id).first()
+    if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
-    user.class_id = request.class_id
-    user.period = request.period
-    user.efitem_id = request.efitem_id
+    db_user.class_id = request.class_id
+    db_user.period = request.period
+    db_user.ef_item_id_1 = request.ef_item_id_1
+    db_user.ef_item_id_2 = request.ef_item_id_2
+    db_user.ef_item_id_3 = request.ef_item_id_3
+    db_user.ef_item_id_4 = request.ef_item_id_4
+    db_user.ef_item_id_5 = request.ef_item_id_5
     db.commit()
+    db.refresh(db_user)
     return {"message": "User profile updated"}
+ 
